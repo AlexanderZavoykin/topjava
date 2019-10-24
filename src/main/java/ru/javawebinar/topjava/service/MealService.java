f61302eb6761ca9c132e89d5658c863aa75ce682
Alexander Zavoykin <aazavoykin@gmail.com>
2019-10-24 16:23:24 +0300

    Added all necessary methods

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -1,9 +1,46 @@
 package ru.javawebinar.topjava.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
+import java.util.List;
+
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
+
+@Service
 public class MealService {
 
     private MealRepository repository;
+
+    public MealService() {
+    }
+
+    @Autowired
+    public MealService(MealRepository repository) {
+        this.repository = repository;
+    }
+
+    public Meal create(Meal meal, int userId) {
+        return repository.save(meal, userId);
+    }
+
+    public void delete(int mealId, int userId) throws NotFoundException {
+        checkNotFoundWithId(repository.delete(mealId, userId), mealId);
+    }
+
+    public Meal get(int mealId, int userId) throws NotFoundException {
+        return checkNotFoundWithId(repository.get(mealId, userId), mealId);
+    }
+
+    public List<Meal> getAll(int userId) {
+        return repository.getAll(userId);
+    }
+
+    public Meal update(Meal meal, int userId) throws NotFoundException {
+        return checkNotFoundWithId(repository.save(meal, userId), meal.getId());
+    }
 
 }
